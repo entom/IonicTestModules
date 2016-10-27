ba0b11692b37ee160af6d93be608801b606dbcef
tnicieja <tomasznicieja@gmail.com>
2016-10-27 22:49:12 +0200

    sms sending

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,56 +1,85 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+    .controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+        // With the new view caching in Ionic, Controllers are only called
+        // when they are recreated or on app start, instead of every page change.
+        // To listen for when this page is active (for example, to refresh data),
+        // listen for the $ionicView.enter event:
+        //$scope.$on('$ionicView.enter', function(e) {
+        //});
 
-  // Form data for the login modal
-  $scope.loginData = {};
+        // Form data for the login modal
+        $scope.loginData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+        // Create the login modal that we will use later
+        $ionicModal.fromTemplateUrl('templates/login.html', {
+            scope: $scope
+        }).then(function (modal) {
+            $scope.modal = modal;
+        });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+        // Triggered in the login modal to close it
+        $scope.closeLogin = function () {
+            $scope.modal.hide();
+        };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+        // Open the login modal
+        $scope.login = function () {
+            $scope.modal.show();
+        };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+        // Perform the login action when the user submits the login form
+        $scope.doLogin = function () {
+            console.log('Doing login', $scope.loginData);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
+            // Simulate a login delay. Remove this and replace with your login
+            // code if using a login system
+            $timeout(function () {
+                $scope.closeLogin();
+            }, 1000);
+        };
+    })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
+    .controller('PlaylistsCtrl', function ($scope) {
+        $scope.playlists = [
+            {title: 'Reggae', id: 1},
+            {title: 'Chill', id: 2},
+            {title: 'Dubstep', id: 3},
+            {title: 'Indie', id: 4},
+            {title: 'Rap', id: 5},
+            {title: 'Cowbell', id: 6}
+        ];
+    })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+    .controller('PlaylistCtrl', function ($scope, $stateParams) {
+
+    })
+
+    .controller('SmsCtrl', function ($scope, $cordovaSms) {
+        $scope.phone = '';
+
+        document.addEventListener("deviceready", function() {
+            var options = {
+                replaceLineBreaks: false, // true to replace \n by a new line, false by default
+                android: {
+                    intent: '' // send SMS with the native android SMS messaging
+                    //intent: '' // send SMS without open any other app
+                    //intent: 'INTENT' // send SMS inside a default SMS app
+                }
+            };
+
+            $scope.sendMessage = function () {
+                $cordovaSms
+                    .send($scope.phone, 'SMS content', options)
+                    .then(function() {
+                        // Success! SMS was sent
+                    }, function(error) {
+                        // An error occurred
+                    });
+            };
+        });
+
+    })
+
+;
